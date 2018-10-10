29c2519ea9eace7f2623fd859073a699f93b0e07
Tim <talbert1209@gmail.com>
2018-10-10 18:44:07 -0500

    Add project files.

---
+++ ConstructorInheritance/MyBaseClass.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace ConstructorInheritance
+{
+    public class MyBaseClass
+    {
+        public MyBaseClass(string baseClassNeedsThis)
+        {
+            MessageBox.Show($@"This is the base class: {baseClassNeedsThis}");
+        }
+    }
+}
